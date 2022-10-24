86606e6c3d2ae52761dd8796d4268a0711451757
SilverLuke <16280387+SilverLuke@users.noreply.github.com>
2022-10-24 23:20:39 +0200

    Update oled
    (cherry picked from commit 990538aa618f91c7537a3c70592da8345dcbb144)

--- keyboards/dactyl_manuform/keymaps/default/oled.c
+++ keyboards/dactyl_manuform/keymaps/default/oled.c
@@ -7,12 +7,24 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 }
 
 bool oled_task_user(void) {
-    // Host Keyboard LED Status
-    //if (is_keyboard_master()) {
-    //    oled_write_P(PSTR("MASTE\n"), false);
-    //} else {
-    //    oled_write_P(PSTR("SLAVE\n"), false);
-    //}
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Layer: "), false);
+
+    switch (get_highest_layer(layer_state)) {
+        case _BASE:
+            oled_write_P(PSTR("BASE\n"), false);
+            break;
+        case _MOVE:
+            oled_write_P(PSTR("MOVE\n"), false);
+            break;
+        case _NUMBER:
+            oled_write_P(PSTR("NUMB\n"), false);
+            break;
+        default:
+            // Or use the write_ln shortcut over adding '\n' to the end of your string
+            oled_write_ln_P(PSTR("Undefined"), false);
+    }
+
     return false;
 }
 #endif
