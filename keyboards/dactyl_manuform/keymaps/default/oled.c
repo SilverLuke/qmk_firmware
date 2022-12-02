3a5ede9a098f04d03773e05a82968a1a365830ce
SilverLuke <16280387+SilverLuke@users.noreply.github.com>
2022-12-02 23:13:40 +0100

    First real test
    (cherry picked from commit 9a8a3efd5b7c0651c9e364e29074abad633a5e6b)

--- keyboards/dactyl_manuform/keymaps/default/oled.c
+++ keyboards/dactyl_manuform/keymaps/default/oled.c
@@ -7,18 +7,55 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 }
 
 bool oled_task_user(void) {
-    // Host Keyboard Layer Status
-    oled_write_P(PSTR("Layer: "), false);
 
     switch (get_highest_layer(layer_state)) {
-        case _BASE:
-            oled_write_P(PSTR("BASE\n"), false);
+        case _LAYER0:
+            oled_write_P(PSTR("WRITE\n"), false);
             break;
-        case _MOVE:
-            oled_write_P(PSTR("MOVE\n"), false);
+        case _LAYER1:
+            oled_write_P(PSTR("NUMBER\n"), false);
             break;
-        case _NUMBER:
-            oled_write_P(PSTR("NUMB\n"), false);
+        case _LAYER2:
+            oled_write_P(PSTR("MOVMENT\n"), false);
+            break;
+        case _LAYER3:
+            oled_write_P(PSTR("3\n"), false);
+            break;
+        case _LAYER4:
+            oled_write_P(PSTR("4\n"), false);
+            break;
+        case _LAYER5:
+            oled_write_P(PSTR("5\n"), false);
+            break;
+        case _LAYER6:
+            oled_write_P(PSTR("6\n"), false);
+            break;
+        case _LAYER7:
+            oled_write_P(PSTR("7\n"), false);
+            break;
+        case _LAYER8:
+            oled_write_P(PSTR("DVORAK\n"), false);
+            break;
+        case _LAYER9:
+            oled_write_P(PSTR("9\n"), false);
+            break;
+        case _LAYER10:
+            oled_write_P(PSTR("10\n"), false);
+            break;
+        case _LAYER11:
+            oled_write_P(PSTR("11\n"), false);
+            break;
+        case _LAYER12:
+            oled_write_P(PSTR("12\n"), false);
+            break;
+        case _LAYER13:
+            oled_write_P(PSTR("13\n"), false);
+            break;
+        case _LAYER14:
+            oled_write_P(PSTR("14\n"), false);
+            break;
+        case _LAYER15:
+            oled_write_P(PSTR("15\n"), false);
             break;
         default:
             // Or use the write_ln shortcut over adding '\n' to the end of your string
